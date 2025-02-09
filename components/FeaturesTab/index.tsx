ab17d9a10855285be004a8fa1eed4f501b4e612b
DarkInventor <ktmehta25@gmail.com>
2025-02-08 22:01:47 -0500

    make dots visible in dark theme

--- components/FeaturesTab/index.tsx
+++ components/FeaturesTab/index.tsx
@@ -61,7 +61,7 @@ const FeaturesTab = () => {
                   01
                 </p>
               </div> */}
-               <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-yellow-500 text-white dark:border-strokedark dark:bg-blacksection">
+               <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-yellow-500 text-white dark:border-strokedark dark:bg-yellow-500">
                 {/* <p className="text-metatitle3 font-medium">03</p> */}
               </div>
               <div className="md:w-3/5 lg:w-auto">
@@ -83,7 +83,7 @@ const FeaturesTab = () => {
                   02
                 </p>
               </div> */}
-               <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-red-500 text-white dark:border-strokedark dark:bg-blacksection">
+               <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-red-500 text-white dark:border-strokedark dark:bg-red-500">
                 {/* <p className="text-metatitle3 font-medium">03</p> */}
               </div>
               <div className="md:w-3/5 lg:w-auto">
@@ -100,7 +100,7 @@ const FeaturesTab = () => {
                   : ""
               }`}
             >
-              <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-blue-500 text-white dark:border-strokedark dark:bg-blacksection">
+              <div className="flex h-12.5 w-12.5 items-center justify-center rounded-[50%] border border-stroke bg-blue-500 text-white dark:border-strokedark dark:bg-blue-500">
                 {/* <p className="text-metatitle3 font-medium">03</p> */}
               </div>
               <div className="md:w-3/5 lg:w-auto">
