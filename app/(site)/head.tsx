ed52f550b4b0c19d7d5e3523c5c5d86021dbb2a1
DarkInventor <ktmehta25@gmail.com>
2025-02-09 18:05:03 -0500

    add metadata

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,10 +1,47 @@
+// export default function Head() {
+//   return (
+//     <>
+//       <title> Solid | Next.js Template for Startup and SaaS</title>
+//       <meta content="width=device-width, initial-scale=1" name="viewport" />
+//       <meta name="description" content="Built with Next.js and TypeScript" />
+//       <link rel="icon" href="/images/favicon.ico" />
+//     </>
+//   );
+// }
+
+
 export default function Head() {
   return (
     <>
-      <title> Solid | Next.js Template for Startup and SaaS</title>
+      <title>File Clever | Stress-Free Canadian Tax Filing</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <meta
+        name="description"
+        content="Maximize your refund with expert remote tax services. File your taxes effortlessly from home. Our Canadian tax experts ensure accuracy, maximize credits, and secure your best possible refund. Simple, reliable, and tailored for you."
+      />
+      <link rel="icon" href="/logo-dark.png" />
+      <meta
+        name="keywords"
+        content="Canadian tax filing, remote tax services, maximize tax refund, expert tax filing, File Clever, stress-free taxes, Canadian tax experts"
+      />
+      <meta property="og:title" content="File Clever | Stress-Free Canadian Tax Filing" />
+      <meta
+        property="og:description"
+        content="Maximize your refund with expert remote tax services. File your taxes effortlessly from home. Our Canadian tax experts ensure accuracy, maximize credits, and secure your best possible refund."
+      />
+      <meta property="og:url" content="https://fileclever.com" />
+      <meta property="og:site_name" content="File Clever" />
+      <meta property="og:image" content="/logo-dark.png" />
+      <meta property="og:locale" content="en_CA" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="File Clever | Stress-Free Canadian Tax Filing" />
+      <meta
+        name="twitter:description"
+        content="Maximize your refund with expert remote tax services. File your taxes effortlessly from home. Our Canadian tax experts ensure accuracy and secure your best possible refund."
+      />
+      <meta name="twitter:image" content="/logo-dark.png" />
     </>
-  );
+  )
 }
+
